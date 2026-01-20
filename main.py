74a8e267ff851574a180a0680d67cf23a4f9f8fa
UncleBParker <bparker091924@gmail.com>
2026-01-20 10:22:16 -0600

    CH1:L5 implemented user_prompt arg and refined error handling

--- main.py
+++ main.py
@@ -1,35 +1,47 @@
 import os
+import argparse
 from dotenv import load_dotenv
 from google import genai
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
-if api_key == None:
-    raise RuntimeError(f"api-key == None")
-    
+if not api_key:
+    raise RuntimeError(
+        "GEMINI_API_KEY environment variable not set. "
+        ".env file or GEMINI_API_KEY object missing"
+    )
 client = genai.Client(api_key=api_key)
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Bootdev Chatbot")
+    parser.add_argument("user_prompt", type=str, help="Prompt to send to Gemini")
+    return parser.parse_args()
 
 def main():
     
-    print("Hello from ai-agent!")
+    print("\nRunning ai-agent!")
 
-    contents = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    args = parse_args()
+    contents = args.user_prompt
 
-    response = client.models.generate_content(model='gemini-2.5-flash', contents=contents)
+    print(f"User Prompt: {contents}\n")
+
+    try:
+        response = client.models.generate_content(
+            model="gemini-2.5-flash",
+            contents=contents,
+        )
+    except Exception as e:
+        raise RuntimeError(f"Gemini API request failed: {e}") from e
     
     usage_metadata = response.usage_metadata
-    if usage_metadata == None:
-        raise RuntimeError("usage_metadata == None / Failed API Request")
     
     prompt_token_count = usage_metadata.prompt_token_count
     canidates_token_count = usage_metadata.candidates_token_count
 
-    print(f"User Prompt: {contents}")
-
     print(f"Prompt tokens: {prompt_token_count}")
     print(f"Response tokens: {canidates_token_count}")
-
-    print(f"Response:\n{response.text}")
+    print(f"Response:\n{response.text}\n")
 
 
 
